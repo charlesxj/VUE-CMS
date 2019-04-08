01cf775f9e65d21292b3fd338d62d2257604c0cc
肖健 <xiaojian@travelsky.com>
2019-04-08 11:30:18 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import './lib/mui/css/mui.css'
+
+import app from './App.vue'
+
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header);
+
+var vm = new Vue({
+  el: '#app',
+  render: a => a(app)
+});
